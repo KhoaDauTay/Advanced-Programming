271fec650ca6c50dda6c766c9c15b59d1bc20b8b
Huynh Tan Khoa <cuonglieu3256@gmail.com>
2020-08-08 12:14:31 +0700

    Add Programm for Abstraction Lab

---
+++ first-program/first-program/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintRow(4);
+            Console.ReadLine();
+        }
+
+        static void PrintRow(int r)
+        {
+            int i, j;
+            for (i = 0; i <= r; i++)
+            {
+                for (j = 1; j <= r - i; j++)
+                    Console.Write(" ");
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+
+            for (i = r - 1; i >= 1; i--)
+            {
+                for (j = 1; j <= r - i; j++)
+                    Console.Write(" ");
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+    }
+ 
+}
